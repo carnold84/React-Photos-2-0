d6df962047bd3c03dff6bdd44621e94b664e6665
c-arnold <chris.wittyname@gmail.com>
2021-06-02 16:06:36 +1200

    Fixed paths

--- src/App.js
+++ src/App.js
@@ -10,154 +10,177 @@ import Image from './components/Image';
 import Slide from './components/Slide';
 
 const Container = styled.div`
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    font-size: 14px;
-    line-height: 18px;
-    color: rgba(0, 0, 0, 0.8);
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    display: flex;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  font-size: 14px;
+  line-height: 18px;
+  color: rgba(0, 0, 0, 0.8);
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  display: flex;
 `;
 
 const Content = styled.div`
-    position: relative;
-    width: 100%;
-    flex-grow: 1;
-    flex-direction: row;
-    align-items: flex-start;
-    justify-content: flex-start;
-    display: flex;
+  position: relative;
+  width: 100%;
+  flex-grow: 1;
+  flex-direction: row;
+  align-items: flex-start;
+  justify-content: flex-start;
+  display: flex;
 `;
 
 class App extends Component {
-    static APP_STATES = {
-      ALBUMS: 'albums',
-      PHOTOS: 'photos',
-      PHOTO: 'photo',
-    };
+  static APP_STATES = {
+    ALBUMS: 'albums',
+    PHOTOS: 'photos',
+    PHOTO: 'photo',
+  };
 
-    state = {
-      view: App.APP_STATES.ALBUMS,
-      albums: undefined,
-      albumsById: undefined,
-      albumId: undefined,
-      photoId: undefined,
-    };
+  state = {
+    view: App.APP_STATES.ALBUMS,
+    albums: undefined,
+    albumsById: undefined,
+    albumId: undefined,
+    photoId: undefined,
+  };
 
-    onBackClick = () => {
-      let { view } = this.state;
+  onBackClick = () => {
+    let { view } = this.state;
 
-      if (view === App.APP_STATES.PHOTOS) {
-        view = App.APP_STATES.ALBUMS;
-      } else if (view === App.APP_STATES.PHOTO) {
-        view = App.APP_STATES.PHOTOS;
+    if (view === App.APP_STATES.PHOTOS) {
+      view = App.APP_STATES.ALBUMS;
+    } else if (view === App.APP_STATES.PHOTO) {
+      view = App.APP_STATES.PHOTOS;
+    }
+
+    this.setState({ view });
+  };
+
+  onImageClick = (id) => {
+    let { view, albumId, photoId } = this.state;
+
+    if (view === App.APP_STATES.ALBUMS) {
+      albumId = id;
+      view = App.APP_STATES.PHOTOS;
+    } else if (view === App.APP_STATES.PHOTOS) {
+      photoId = id;
+      view = App.APP_STATES.PHOTO;
+    }
+
+    this.setState({ view, albumId, photoId });
+  };
+
+  createImages = (data, onClick) => {
+    // loop through data and create an Image element for each one
+    let items = data.map((item) => {
+      let image_url = undefined;
+
+      // check if photos exist
+      if (item.photos !== undefined && item.photos.length > 0) {
+        // store image url of first photo if it's album (has a photos attribute)
+        image_url = item.photos[0].url;
+      } else if (item.url !== undefined) {
+        // no photos attribute so it's a photo
+        image_url = item.url;
       }
 
-      this.setState({ view });
-    };
+      // create Image element
+      return (
+        <Image
+          id={item.id}
+          title={item.title}
+          imageUrl={image_url}
+          onClick={onClick}
+          key={item.id}
+        />
+      );
+    });
 
-    onImageClick = id => {
-      let { view, albumId, photoId } = this.state;
+    return items;
+  };
+
+  componentDidMount() {
+    const { source } = this.props;
+
+    // use the built in fetch api to retrieve JSON file
+    fetch(source)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        const albums = data.albums;
+        let albumsById = {};
+        const nextAlbums = albums.map((album) => {
+          const nextAlbum = {
+            ...album,
+            photos: album.photos.map((photo) => {
+              return {
+                ...photo,
+                url: `${process.env.PUBLIC_URL}/${photo.url}`,
+              };
+            }),
+          };
+          albumsById[album.id] = nextAlbum;
+
+          return nextAlbum;
+        });
+        // ... and update the state
+        this.setState({ albums: nextAlbums, albumsById });
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+
+  render() {
+    const { albums, albumsById, albumId, photoId, view } = this.state;
+
+    let container = undefined;
+    let onBackClick = undefined;
+
+    if (albums) {
+      let content = undefined;
 
       if (view === App.APP_STATES.ALBUMS) {
-        albumId = id;
-        view = App.APP_STATES.PHOTOS;
+        const items = this.createImages(albums, this.onImageClick);
+        content = <Grid>{items}</Grid>;
       } else if (view === App.APP_STATES.PHOTOS) {
-        photoId = id;
-        view = App.APP_STATES.PHOTO;
+        const items = this.createImages(
+          albumsById[albumId].photos,
+          this.onImageClick
+        );
+        content = <Grid>{items}</Grid>;
+        onBackClick = this.onBackClick;
+      } else if (view === App.APP_STATES.PHOTO) {
+        const photo = _find(
+          albumsById[albumId].photos,
+          (photo) => photo.id === photoId
+        );
+        content = (
+          <Slide id={photo.id} title={photo.title} imageUrl={photo.url} />
+        );
+        onBackClick = this.onBackClick;
       }
 
-      this.setState({ view, albumId, photoId });
-    };
-
-    createImages = (data, onClick) => {
-      // loop through data and create an Image element for each one
-      let items = data.map(item => {
-        let image_url = undefined;
-
-        // check if photos exist
-        if (item.photos !== undefined && item.photos.length > 0) {
-          // store image url of first photo if it's album (has a photos attribute)
-          image_url = item.photos[0].url;
-        } else if (item.url !== undefined) {
-          // no photos attribute so it's a photo
-          image_url = item.url;
-        }
-
-        // create Image element
-        return <Image id={item.id} title={item.title} imageUrl={image_url} onClick={onClick} key={item.id} />;
-      });
-
-      return items;
-    };
-
-    componentDidMount() {
-      const { source } = this.props;
-
-      // use the built in fetch api to retrieve JSON file
-      fetch(source)
-        .then(response => {
-          return response.json();
-        })
-        .then(data => {
-          const albums = data.albums;
-          let albumsById = {};
-          albums.forEach(album => {
-            albumsById[album.id] = album;
-          });
-          // ... and update the state
-          this.setState({ albums, albumsById });
-        })
-        .catch(err => {
-          console.error(err);
-        });
+      container = (
+        <Container>
+          <Actionbar title={'React Photos 2.0'} onBackClick={onBackClick} />
+          <Content>{content}</Content>
+        </Container>
+      );
+    } else {
+      container = (
+        <Container>
+          <Loading />
+        </Container>
+      );
     }
 
-    render() {
-      const { albums, albumsById, albumId, photoId, view } = this.state;
-
-      let container = undefined;
-      let onBackClick = undefined;
-
-      if (albums) {
-
-        let content = undefined;
-
-        if (view === App.APP_STATES.ALBUMS) {
-          const items = this.createImages(albums, this.onImageClick);
-          content = <Grid>{items}</Grid>;
-        } else if (view === App.APP_STATES.PHOTOS) {
-          const items = this.createImages(albumsById[albumId].photos, this.onImageClick);
-          content = <Grid>{items}</Grid>;
-          onBackClick = this.onBackClick;
-        } else if (view === App.APP_STATES.PHOTO) {
-          const photo = _find(albumsById[albumId].photos, photo => photo.id === photoId);
-          content = <Slide id={photo.id} title={photo.title} imageUrl={photo.url} />;
-          onBackClick = this.onBackClick;
-        }
-            
-        container = (
-          <Container>
-            <Actionbar title={'React Photos 2.0'} onBackClick={onBackClick} />
-            <Content>
-              {content}
-            </Content>
-          </Container>
-        );
-
-      } else {
-        container = (
-          <Container>
-            <Loading />
-          </Container>
-        );
-      }
-
-      return container;
-    }
+    return container;
+  }
 }
 
 const { string } = PropTypes;
--- src/index.js
+++ src/index.js
@@ -4,5 +4,8 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App source="sample_data.json" />, document.getElementById('root'));
+ReactDOM.render(
+  <App source={`${process.env.PUBLIC_URL}/sample_data.json`} />,
+  document.getElementById('root')
+);
 registerServiceWorker();
